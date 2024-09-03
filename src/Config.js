af70fb570de29b8ef36ee92324e460b6ffcc9473
Kakarla V S Ravindra Reddy <kvsravindrareddy@gmail.com>
2024-09-02 23:10:16 -0400

    geo changes

--- src/Config.js
+++ src/Config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
     development: {
-        EVALUATION_BASE_URL: 'http://69.127.132.13:9004',
-        ADMIN_BASE_URL: 'http://69.127.132.13:9003',
-        AI_ASSESSMENT_BASE_URL: 'http://69.127.132.13:9005'
+        EVALUATION_BASE_URL: 'http://98.111.198.125:9004',
+        ADMIN_BASE_URL: 'http://98.111.198.125:9003',
+        AI_ASSESSMENT_BASE_URL: 'http://98.111.198.125:9005'
     },
     staging: {
-        EVALUATION_BASE_URL: 'http://69.127.132.13:9004',
-        ADMIN_BASE_URL: 'http://69.127.132.13:9003',
-        AI_ASSESSMENT_BASE_URL: 'http://69.127.132.13:9005'
+        EVALUATION_BASE_URL: 'http://98.111.198.125:9004',
+        ADMIN_BASE_URL: 'http://98.111.198.125:9003',
+        AI_ASSESSMENT_BASE_URL: 'http://98.111.198.125:9005'
     },
     production: {
-        EVALUATION_BASE_URL: 'http://69.127.132.13:9004',
-        ADMIN_BASE_URL: 'http://69.127.132.13:9003',
-        AI_ASSESSMENT_BASE_URL: 'http://69.127.132.13:9005'
+        EVALUATION_BASE_URL: 'http://98.111.198.125:9004',
+        ADMIN_BASE_URL: 'http://98.111.198.125:9003',
+        AI_ASSESSMENT_BASE_URL: 'http://98.111.198.125:9005'
     }
 };
 
---
+++ src/pages/geo/GeolocationComponent.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+function GeolocationComponent() {
+  const [location, setLocation] = useState({ latitude: null, longitude: null });
+  const [error, setError] = useState(null);
+
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          });
+        },
+        (error) => {
+          setError(error.message);
+        }
+      );
+    } else {
+      setError("Geolocation is not supported by this browser.");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Find My Location</h2>
+      <button onClick={getLocation}>Get Location</button>
+      {location.latitude && location.longitude && (
+        <p>
+          Latitude: {location.latitude} <br />
+          Longitude: {location.longitude}
+        </p>
+      )}
+      {error && <p>Error: {error}</p>}
+    </div>
+  );
+}
+
+export default GeolocationComponent;
